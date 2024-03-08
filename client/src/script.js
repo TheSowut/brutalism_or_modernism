255e4e35b6c7a6360fbf05d5a92e962cca35a0ef
TheSowut <sowutmail@gmail.com>
2024-03-08 18:30:51 +0200

    style: loading state

--- client/src/script.js
+++ client/src/script.js
@@ -15,26 +15,45 @@ var TEXT;
 (function (TEXT) {
     TEXT["DEFAULT"] = "Drop \uD83C\uDFDB\uFE0F image";
     TEXT["SUCCESS"] = "\u2705";
+    TEXT["LOADING"] = "\u23F3";
 })(TEXT || (TEXT = {}));
 var handleClickImport = function () {
     alert(123);
 };
+/**
+ * Handle drop of picture.
+ *
+ * @param event
+ */
 var dropHandler = function (event) {
     event.preventDefault();
-    box.style.outlineStyle = 'none';
-    text.innerHTML = TEXT.DEFAULT;
+    box.style.outlineColor = COLOR_SCHEME.WARNING;
+    text.innerHTML = TEXT.LOADING;
 };
+/**
+ * Handle dragging of picture over element.
+ *
+ * @param event
+ */
 var dragOverHandler = function (event) {
     event.preventDefault();
     box.style.outlineColor = COLOR_SCHEME.SUCCESS;
     box.style.outlineStyle = 'solid';
     text.innerHTML = TEXT.SUCCESS;
 };
+/**
+ * Handle dragging out of element.
+ *
+ * @param event
+ */
 var dragLeaveHandler = function (event) {
     event.preventDefault();
     box.style.outlineStyle = 'none';
     text.innerHTML = TEXT.DEFAULT;
 };
+/**
+ * Load HTML Elements.
+ */
 window.addEventListener('load', function () {
     // document.querySelector('#btnTest')?.addEventListener('click', async () => {
     //     await fetch('http://www.localhost:8080/test')
--- client/src/script.ts
+++ client/src/script.ts
@@ -14,20 +14,32 @@ enum COLOR_SCHEME {
 
 enum TEXT {
     'DEFAULT' = 'Drop 🏛️ image',
-    'SUCCESS' = '✅'
+    'SUCCESS' = '✅',
+    'LOADING' = '⏳',
 }
 
 const handleClickImport = () => {
     alert(123);
 }
 
+/**
+ * Handle drop of picture.
+ *
+ * @param event
+ */
 const dropHandler = (event: any) => {
     event.preventDefault();
 
-    box.style.outlineStyle = 'none';
-    text.innerHTML = TEXT.DEFAULT;
+    // Loading begins
+    box.style.outlineColor = COLOR_SCHEME.WARNING;
+    text.innerHTML = TEXT.LOADING;
 }
 
+/**
+ * Handle dragging of picture over element.
+ *
+ * @param event
+ */
 const dragOverHandler = (event: any) => {
     event.preventDefault();
 
@@ -36,13 +48,21 @@ const dragOverHandler = (event: any) => {
     text.innerHTML = TEXT.SUCCESS;
 }
 
+/**
+ * Handle dragging out of element.
+ *
+ * @param event
+ */
 const dragLeaveHandler = (event: any) => {
-        event.preventDefault();
+    event.preventDefault();
 
-        box.style.outlineStyle = 'none';
-        text.innerHTML = TEXT.DEFAULT;
+    box.style.outlineStyle = 'none';
+    text.innerHTML = TEXT.DEFAULT;
 }
 
+/**
+ * Load HTML Elements.
+ */
 window.addEventListener('load', () => {
     // document.querySelector('#btnTest')?.addEventListener('click', async () => {
     //     await fetch('http://www.localhost:8080/test')
